35fdb8a89d792fba799b4fb48ec6d8e8ef3b1836
imrulhassanemon <imrulhassan989@gmail.com>
2024-12-19 00:15:22 +0600

    added picture animaiton

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,20 +1,55 @@
 import { easeIn } from "motion";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
+import team1 from "../../../public/team/team01.jpg";
+import team2 from "../../../public/team/team02.jpg";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
+        <div className="flex-1">
+          <motion.img
+            src={team1}
+            animate={{y:[50, 100, 50]}}
+            transition={{duration:10, repeat:Infinity}}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600 shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            animate={{x:[100, 150, 100]}}
+            transition={{duration:10, repeat:Infinity}}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-600 shadow-2xl"
+          />
+        </div>
+        <div className="flex-1">
           <motion.h1
-          animate={{x:50}}
-          transition={{duration:1, delay:2, ease: easeIn, repeat: Infinity}}
-
-          className="text-5xl font-bold">Latest Jobs For You!</motion.h1>
+            animate={{ x: 100 }}
+            transition={{
+              duration: 3,
+              delay: 1,
+              ease: easeIn,
+              repeat: Infinity,
+            }}
+            className="text-5xl font-bold"
+          >
+            Latest{" "}
+            <motion.span
+              animate={{
+                color: [
+                  "#2ea7d1",
+                  "#2368dc",
+                  "#7e1ae5",
+                  "#be1be4",
+                  "#c94436",
+                  "#d39c2c",
+                ],
+              }}
+              transition={{ repeat: Infinity }}
+            >
+              Jobs
+            </motion.span>{" "}
+            For You!
+          </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
